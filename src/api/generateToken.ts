d3fc6ad1cb10dbbd3dbbdc5860f379ff13fe058d
Mohammad Rizky Pratama <rizkymrp22@gmail.com>
2023-08-22 12:33:39 +0700

    update config data generate token

--- src/api/generateToken.ts
+++ src/api/generateToken.ts
@@ -1,15 +1,12 @@
 import axios from 'axios';
+import generateTokenData from '../data/generateToken.data';
 
 export async function generateToken() {
-    const data = {
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET
-    };
-  
-    const response = await axios.post(process.env.BASE_URL + '/users-management/v3/auth/generatetoken', data);
-    if (response.status === 200) {
-      return response;
-    } else {
-      throw new Error('Token generation failed');
-    }
+
+  const response = await axios.post(process.env.BASE_URL + '/users-management/v3/auth/generatetoken', generateTokenData);
+  if (response.status === 200) {
+    return response;
+  } else {
+    throw new Error('Token generation failed');
   }
+}
---
+++ src/data/generateToken.data.ts
@@ -0,0 +1,6 @@
+const generateToken = {
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET
+  };
+
+  export default{generateToken}
